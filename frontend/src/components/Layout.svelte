8f330a4e6df1f7da2dfafec399b5c3137fad743a
Siaka Joe Sirleaf <humblepleaser@icloud.com>
2022-11-07 00:28:07 +0800

    configure layout

---
+++ frontend/src/components/Layout.svelte
@@ -0,0 +1,50 @@
+<script>
+
+    import {
+      Header,
+      HeaderNav,
+      HeaderNavItem,
+      HeaderNavMenu,
+      SkipToContent,
+      Content,
+      Grid,
+      Row,
+      Column,
+    } from "carbon-components-svelte";
+  
+    let isSideNavOpen = false;
+    import '../styles/index.scss';
+  </script>
+  
+  <Header company="IBM" platformName="Carbon Svelte" bind:isSideNavOpen>
+    <svelte:fragment slot="skip-to-content">
+      <SkipToContent />
+    </svelte:fragment>
+    <HeaderNav>
+      <HeaderNavItem href="/" text="FAQ" />
+  
+      <HeaderNavMenu text="Currency">
+        <HeaderNavItem href="/" text="USD" />
+        <HeaderNavItem href="/" text="LRD" />
+        
+      </HeaderNavMenu>
+
+      <HeaderNavMenu text="Locations">
+        <HeaderNavItem href="/" text="Red Light" />
+        <HeaderNavItem href="/" text="Waterside" />
+        <HeaderNavItem href="/" text="Duala" />
+        
+      </HeaderNavMenu>
+    </HeaderNav>
+  </Header>
+  
+  <Content>
+    <Grid>
+      <Row>
+        <Column>
+        <slot/>
+        </Column>
+      </Row>
+    </Grid>
+  </Content>
+  
