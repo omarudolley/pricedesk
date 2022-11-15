ad70fd5f049fdf77bb9223dcff198e4c3a124bc3
Siaka Joe Sirleaf <humblepleaser@icloud.com>
2022-11-16 05:17:20 +0800

    faqs update

--- frontend/src/lib/data.ts
+++ frontend/src/lib/data.ts
@@ -2,49 +2,37 @@ import type { FAQ } from '$lib/types'
 
 export const faqs: FAQ[] = [
   {
-    
     question: 'What is Liberia Market Index?',
-    answer: `Explain about Liberia Market Index here`,
+    answer: `Market Index provides accurate and up-to-date information about commodiries on the Liberian market.`,
   },
   {
-
     question: 'When is data updated?',
     answer: 'Data is update twice a week (Mondays and Thursdays',
   },
 
   {
-
     question: 'Where does the data come from?',
     answer: 'Data are collected in real time by our agents in the market',
   },
-  {
-
-    question: 'Who build the app?',
-    answer: 'The app was built by Solo Omaru Dolley and Siaka Joe Sirleaf of TarjahTech',
-  },
-  {
-
-    question: 'Who manages the app?',
-    answer: 'The app is being manage by TarjahTech',
-  },
-  {
-
-    question: 'I have a feature request , where can i submit it?',
-    answer: 'You can submit your feature request to tarjahtech@gmail.com',
-  },
-  {
   
+  {
+    question: 'Who manages the app?',
+    answer: 'The app is managed by TarjahTech',
+  },
+  {
+    question: 'I have a feature request , where can i submit it?',
+    answer: 'Proposals for additional features are welcome on our Github - https://github.com/omarudolley/pricedesk/issues',
+  },
+  {
     question: 'Where can I support the application?',
     answer: 'you can send support to any of the following accounts: Paypal: humblepleaser@icloud.com; MTN Mobile Money: 0886295272',
   },
   {
-
     question: 'Who can I contact for more information?',
-    answer: 'For more information contact Siaka Joe Sirleaf,email: humblepleaser@icloud.com; Whatsapp: +8615857983179',
+    answer: 'For more information contact Siaka Joe Sirleaf. Email: humblepleaser@icloud.com; Whatsapp: +8615857983179',
 
   }
 ]
-
 export const commodities: string[] = [
   'USD buying rate',
   'USD selling rate',
