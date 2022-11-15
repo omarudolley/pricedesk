91d83e0401e82cb684c1fd73f3fae2cf133f4822
Shatino94 <71574829+Shatino94@users.noreply.github.com>
2022-11-16 05:22:13 +0800

    Update faq (#5)
    * faqs
    
    * faqs update
    
    Co-authored-by: Siaka Joe Sirleaf <humblepleaser@icloud.com>

--- frontend/src/routes/faq/+page.svelte
+++ frontend/src/routes/faq/+page.svelte
@@ -5,7 +5,7 @@
 
 <div class="container">
   <div class="content">
-    <h2> Frequestly asked questions: </h2>
+    <h2 class="header"> Frequestly asked questions: </h2>
     <Accordion size="xl">
       {#each faqs as item}
         <AccordionItem title={item.question}>
@@ -23,10 +23,15 @@
     display: flex;
     flex-direction: column;
 
+    .header{
+      margin-bottom: 2rem;
+    }
+
     .content {
       display: flex;
       flex-direction: column;
       max-width: $container-max-width;
+
       .item {
         padding: 1.5rem;
       }
