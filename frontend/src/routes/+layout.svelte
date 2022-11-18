2f1d6754367215963a3799c9f39174cf27d303b1
Solo <solodolley@gmail.com>
2022-11-18 09:17:32 -0800

    add og image

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script>
   import Layout from '$components/Layout.svelte'
   import SEO from '$components/SEO.svelte'
+  import Image from '$lib/assets/og.jpg'
 </script>
 
 <SEO
   description="Market Index aims to provide accurate and up-to-date information about the price of basis
 commodities in Liberia."
   keywords="Liberia, Market, prices, commodities, inflation"
+  image={Image}
 />
 <Layout>
   <slot />
